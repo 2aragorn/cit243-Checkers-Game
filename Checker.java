8b40b371cded028127915aff419d7c8a9f4cecf1
mrcliffbailey <mrcliffbailey@gmail.com>
2019-09-17 13:30:31 -0500

    Create Checker.java

---
+++ Checker.java
@@ -0,0 +1,24 @@
+public class Checker {
+	
+  private String player;
+  private bool crowned;
+
+
+  public Checker(String player) {
+    this.player = player;
+    crowned = false;
+  }
+  
+  public String GetPlayer() {
+    return player;
+  }
+  
+  public String GetCrowned() {
+    return crowned;
+  }
+  
+  public void SetCrowned() {
+    crowned = true;
+  }
+  
+}
