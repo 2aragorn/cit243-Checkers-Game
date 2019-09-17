8a8229653d0aaa69cba9eb6dd7397c94886ddc47
mrcliffbailey <mrcliffbailey@gmail.com>
2019-09-17 13:31:02 -0500

    Update Checker.java

--- Checker.java
+++ Checker.java
@@ -1,24 +1,24 @@
 public class Checker {
 	
-  private String player;
-  private bool crowned;
+	private String player;
+	private bool crowned;
 
 
-  public Checker(String player) {
-    this.player = player;
-    crowned = false;
-  }
-  
-  public String GetPlayer() {
-    return player;
-  }
-  
-  public String GetCrowned() {
-    return crowned;
-  }
-  
-  public void SetCrowned() {
-    crowned = true;
-  }
+	public Checker(String player) {
+		this.player = player;
+		crowned = false;
+	}
+
+	public String GetPlayer() {
+		return player;
+	}
+
+	public String GetCrowned() {
+		return crowned;
+	}
+
+	public void SetCrowned() {
+		crowned = true;
+	}
   
 }
