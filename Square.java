5dc5ad0d24d001c8b961df26948be2319010dc3f
mrcliffbailey <mrcliffbailey@gmail.com>
2019-09-17 13:43:16 -0500

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,8 +1,35 @@
 
 public class Square {
-
-	public Square() {
 	
+	private String color;
+	private bool occupied;
+	private Checker checker;
+
+	public Square(String color) {
+		this.color = color;
+		occupied = false;
+	}
+	
+	public String GetColor() {
+		return color;
+	}
+	
+	public bool IsOccupied() {
+		return occupied;
+	}
+	
+	public Checker GetChecker() {
+		return checker;
+	}
+	
+	public void SetChecker(Checker checker) {
+		occupied = true;
+		this.checker = checker;
+	}
+	
+	public void RemoveChecker() {
+		occupied = false;
+		checker = null;
 	}
 
 }
