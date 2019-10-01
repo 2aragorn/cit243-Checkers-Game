4cf4e653d329ae72a9b2f5cfc1e72ab1df9d20a9
mrcliffbailey <mrcliffbailey@gmail.com>
2019-10-01 13:35:02 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -5,7 +5,67 @@ public class Game {
 
 	public Game() {
 		players = new String[]{"Red", "Black"};
-		board = new Board();
 	}
+	
+	
+	public void StartGame() {
+		
+		board = new Board();	// Initialize a new board
+		
+		SetupBoard();
+		GameLoop();
+	}
+		
+	public void GameLoop() {
+		// Incomplete functions
+	}
+	
+	
+	public void EndGame() {
+		// Incomplete functions
+	}
+	
+	
+	
+	// --------------------------------------------------------
+	// Setup functions	
+	// --------------------------------------------------------
+		
+	private void SetupBoard() {
+		AddRedCheckers();
+		AddBlackCheckers();
+	}
+	
+	private void AddRedCheckers() {
+		// Incomplete functions
+		
+		// There are 12 red checkers added at the top of the board.
+		// REMINDER: counting in arrays starts at 0
+		
+		for (var y = 0; y < 3; y++) {	// Loop down the first 3 rows
+			for (var x = 0; x < 8; x++) {	// Loop across all 8 columns
+				
+				bool xEven = ((x % 2) == 0);	// Check if x is an even number
+				bool yEven = ((y % 2) == 0);	// Check if y is an even number
+				
+				bool isBlack = (xEven == yEven);	// Black squares have matching x and y coordinates
+				
+				
+				if (isBlack) {
+					// 1.) Get this square
+					// 2.) Create a new checker for this player
+					// 3.) Assign this checker to the square
+				}
+				
+			}		
+		}		
+	}
+	
+	private void AddBlackCheckers() {
+		// Incomplete functions
+	}
+	
+		
+	
 	
 }
