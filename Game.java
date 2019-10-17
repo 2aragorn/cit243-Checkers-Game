8fcca1f2e4059ff180ce6f7bc0bc9af96670f765
mrcliffbailey <mrcliffbailey@gmail.com>
2019-10-17 13:41:08 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -36,9 +36,7 @@ public class Game {
 		AddBlackCheckers();
 	}
 	
-	private void AddRedCheckers() {
-		// Incomplete functions
-		
+	private void AddRedCheckers() {		
 		// There are 12 red checkers added at the top of the board.
 		// REMINDER: counting in arrays starts at 0
 		
@@ -52,17 +50,34 @@ public class Game {
 				
 				
 				if (isBlack) {
-					// 1.) Get this square
-					// 2.) Create a new checker for this player
-					// 3.) Assign this checker to the square
+					Checker checker = new Checker("Red");		// Initialize a new RED checker
+					board.squares[x][y].SetChecker(checker);	// Place the checker on the board
 				}
 				
 			}		
 		}		
 	}
 	
-	private void AddBlackCheckers() {
-		// Incomplete functions
+	private void AddBlackCheckers() {	
+		// There are 12 black checkers added at the bottom of the board.
+		// REMINDER: counting in arrays starts at 0 and ends at 7
+		
+		for (var y = 7; y > 4; y--) {	// Loop up the first 3 rows
+			for (var x = 0; x < 8; x++) {	// Loop across all 8 columns
+				
+				bool xEven = ((x % 2) == 0);	// Check if x is an even number
+				bool yEven = ((y % 2) == 0);	// Check if y is an even number
+				
+				bool isBlack = (xEven == yEven);	// Black squares have matching x and y coordinates
+				
+				
+				if (isBlack) {
+					Checker checker = new Checker("Black");		// Initialize a new BLACK checker
+					board.squares[x][y].SetChecker(checker);	// Place the checker on the board
+				}
+				
+			}		
+		}		
 	}
 	
 		
